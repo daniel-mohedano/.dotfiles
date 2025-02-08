8d46f42624c25b977e8382a2348b0002b3c4d3ef
Daniel Mohedano <daniel.mohedano@datadoghq.com>
2025-02-08 12:28:26 +0100

    configure blink (now default on lazyvim)

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,43 +1,23 @@
 return {
   {
-    "nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-cmdline",
-      "rcarriga/cmp-dap",
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- default cmdline sources
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" or type == "@" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+      },
+      completion = { list = { selection = { preselect = false, auto_insert = true } } },
+      keymap = { preset = "default" },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-
-      opts.window = {
-        completion = vim.tbl_deep_extend("force", cmp.config.window.bordered(), {
-          winhighlight = "Normal:Pmenu",
-        }),
-        documentation = vim.tbl_deep_extend("force", cmp.config.window.bordered(), {
-          winhighlight = "Normal:Pmenu",
-        }),
-      }
-
-      cmp.setup.filetype({ "dap-repl", "dapui_watches" }, {
-        sources = { { name = "dap" } },
-      })
-
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
-    end,
   },
   {
     "echasnovski/mini.pairs",
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -12,5 +12,8 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      integrations = { blink_cmp = true },
+    },
   },
 }
